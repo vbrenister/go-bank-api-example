150f0760b5ea9a57c9c9d06de79823ed04782570
Victor Brenister <v.brenister@newmotion.com>
2023-03-10 16:49:02 +0100

    fix deadlock

--- db/query/account.sql
+++ db/query/account.sql
@@ -11,6 +11,10 @@ INSERT INTO accounts (
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1 FOR NO KEY UPDATE;
+
 -- name: ListAccounts :many
 SELECT * FROM accounts
 ORDER BY id
@@ -20,6 +24,11 @@ OFFSET $2;
 -- name: UpdateAccount :one
 UPDATE accounts SET balance = $1 
 WHERE id = $2 
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
